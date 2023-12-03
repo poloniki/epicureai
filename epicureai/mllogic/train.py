7b35d4cefa4f22fc1d1cfbea5fcf3f9886b6f565
Nikita Polovinkin <poloniki@ya.ru>
2023-12-03 21:40:02 +0100

    new

--- epicureai/mllogic/train.py
+++ epicureai/mllogic/train.py
@@ -4,55 +4,71 @@ import os
 import comet_ml
 from comet_ml import API
 
-
+# Set up workspace, model name, and project variables
 workspace = "poloniki"
 model_name = "yolo-model"
 project = "epicure"
 
+# Set an environment variable for the model name
 os.environ["COMET_MODEL_NAME"] = model_name
 
 
+# Function to train the model
 def train_model(epochs: int = 10, img_size: int = 512):
-    # Initialize YOLO connection
+    # Initialize Comet ML API connection
     api = API()
     comet_ml.init()
 
-    # If there are already pretrained weights - take them if not load new ones
+    # Try to use pretrained weights if available
     try:
+        # Fetching the model from Comet ML
         models = api.get_model(workspace=workspace, model_name=model_name)
         model_versions = models.find_versions()
         last_version = model_versions[0]
+
+        # Preparing local path for weights
         weights_path = os.path.join(LOCAL_DATA_PATH, "weights")
         os.makedirs(weights_path, exist_ok=True)
+
+        # Downloading the weights
         models.download(
             version=last_version,
             output_folder=weights_path,
             expand=True,
         )
-        model = YOLO(os.path.join(weights_path, "best.pt"))
-        print("✅ Loaded weights from the comet ML")
-    except Exception as error:
-        print(f"❌ Could not loaded weights: {error}")
-        model = YOLO("yolov8n.pt")
-        print("❗️ Initialized new weights from scratch")
-    # Train the model
-    model.train(
-        data=os.path.join(LOCAL_DATA_PATH, "data.yaml"),
-        epochs=epochs,
-        imgsz=img_size,
-        patience=20,
-    )
 
-    # Saving latest model weights to the COMET ML online
+        # Load the model with the downloaded weights
+        model = YOLO(os.path.join(weights_path, "best.pt"))
+        model.train(resume=True)
+        print("✅ Loaded weights from the comet ML")
+
+    # If loading pretrained weights fails, initialize a new model
+    except Exception as error:
+        print(f"❌ Could not load weights: {error}")
+
+        # Initialize a new YOLO model with default weights
+        model = YOLO("yolov8n.pt")
+        model.train(
+            data=os.path.join(LOCAL_DATA_PATH, "data.yaml"),
+            epochs=epochs,
+            imgsz=img_size,
+            patience=20,
+        )
+        print("❗️ Initialized new weights from scratch")
+
+    # Save the trained model weights to Comet ML
     experiments = api.get(workspace=workspace, project_name=project)
+
+    # Registering the latest experiment and model
     experiment = api.get(
         workspace=workspace,
         project_name=project,
         experiment=experiments[-1]._name,
     )
     experiment.register_model(model_name)
-    print("✅ Save weights of this run to Comet ML")
+    print("✅ Saved weights of this run to Comet ML")
 
 
+# Main execution
 if __name__ == "__main__":
     train_model(epochs=NUM_EPOCHS)
